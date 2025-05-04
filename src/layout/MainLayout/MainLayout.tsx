b97def30c0dc5258cb85da136cda2d392e60bd34
Emmanuel Montenegro <leunamm3@gmail.com>
2025-05-04 00:59:13 -0300

    refactor: allow full-height content

--- src/layout/MainLayout/MainLayout.tsx
+++ src/layout/MainLayout/MainLayout.tsx
@@ -2,11 +2,13 @@ import { Outlet } from 'react-router-dom';
 import { Header } from '../../components/Header/Header';
 import { Footer } from '../../components/Footer/Footer';
 
+import styles from './MainLayout.module.scss';
+
 export const MainLayout = () => {
   return (
-    <div>
+    <div className={styles.layout}>
       <Header />
-      <main>
+      <main className={styles.layout__main}>
         <Outlet />
       </main>
       <Footer />

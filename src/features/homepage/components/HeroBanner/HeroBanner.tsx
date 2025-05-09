601c29222dfbb7cc77bfeaf437b562c259ded0b8
Emmanuel Montenegro <leunamm3@gmail.com>
2025-05-08 23:02:14 -0300

    feat: add button to Hero

--- src/features/homepage/components/HeroBanner/HeroBanner.tsx
+++ src/features/homepage/components/HeroBanner/HeroBanner.tsx
@@ -1,4 +1,5 @@
 import { Container } from '../../../../components/Container';
+import { Button } from '../../../../components/Button';
 
 import styles from './HeroBanner.module.scss';
 
@@ -6,6 +7,9 @@ export const HeroBanner = () => (
   <div className={styles.heroBanner}>
     <Container className={styles.heroBanner__container}>
       <h1 className={styles.heroBanner__title}>Welcome!</h1>
+      <div>
+        <Button size="sm">Shop Now</Button>
+      </div>
     </Container>
   </div>
 );

8bb2f842cc196a292936adafd544e7070a5c87c8
Emmanuel Montenegro <leunamm3@gmail.com>
2025-05-09 21:37:34 -0300

    feat: add elements to Header

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -1,17 +1,27 @@
-import { Link } from 'react-router-dom';
+import clsx from 'clsx';
+import { User, ShoppingCart, Heart, Search } from 'lucide-react';
 import { Container } from '../../components/Container';
 import styles from './Header.module.scss';
 
 export const Header = () => (
-  <header className={styles.header}>
-    <Container className={styles.header__container}>
-      <div className={styles.header__logo}>LOGO</div>
-      <nav className={styles.header__nav}>
-        <button className={styles.header__icon}>🛒</button>
-        <Link to="/my-account">
-          <button className={styles.header__icon}>👤</button>
-        </Link>
-      </nav>
-    </Container>
+  <header>
+    <div className={styles.banner}>
+      <Container className={styles.banner__container}>
+        <div className={clsx(styles.banner__column, styles['banner__column--left'])}>1</div>
+        <div className={clsx(styles.banner__column, styles['banner__column--center'])}>2</div>
+        <div className={clsx(styles.banner__column, styles['banner__column--right'])}>3</div>
+      </Container>
+    </div>
+    <div className={styles.navbar}>
+      <Container className={styles.navbar__container}>
+        <div className={styles.navbar__logo}>E-commerce</div>
+        <nav className={styles.navbar__nav}>
+          <User className={styles.navbar__nav__icon} />
+          <Search className={styles.navbar__nav__icon} />
+          <ShoppingCart className={styles.navbar__nav__icon} />
+          <Heart className={styles.navbar__nav__icon} />
+        </nav>
+      </Container>
+    </div>
   </header>
 );

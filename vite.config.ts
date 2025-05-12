2e01e2f220c51900a8e5662552ba0cf48642a3dd
Emmanuel Montenegro <leunamm3@gmail.com>
2025-05-12 20:50:38 -0300

    chore: add vite-plugin-svgr library

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig, UserConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr({
+      include: '**/*.svg?react',
+    }),
+  ],
   css: {
     modules: {
       generateScopedName: (name, filename) => {

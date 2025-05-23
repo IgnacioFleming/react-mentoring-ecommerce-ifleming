85ac8d4ba028760edbdb6e2513c510e317268eb5
Emmanuel Montenegro <leunamm3@gmail.com>
2025-05-23 18:55:25 -0300

    feat: add background color to Shop

--- src/pages/Shop/Shop.tsx
+++ src/pages/Shop/Shop.tsx
@@ -1,14 +1,19 @@
 import { Container } from '../../components/Container';
 import { useProductCategories } from '../../features/shop/hooks/useProductCategories';
 import { Categories } from '../../features/shop/components/Categories';
+import styles from './Shop.module.scss';
 
 export const Shop = () => {
   const { productCategories } = useProductCategories();
 
   return (
-    <Container>
-      <h2>Shop</h2>
-      <Categories productCategories={productCategories || []} />
-    </Container>
+    <div className={styles.shop}>
+      <div className={styles.shop__header}>
+        <Container>
+          <h2>Shop</h2>
+          <Categories productCategories={productCategories || []} />
+        </Container>
+      </div>
+    </div>
   );
 };

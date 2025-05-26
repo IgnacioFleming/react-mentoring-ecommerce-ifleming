eb8b792141aaf8e9988403bcbaf7e4eb589000fc
Emmanuel Montenegro <leunamm3@gmail.com>
2025-05-26 19:41:18 -0300

    test: update Button unit tests

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -6,33 +6,34 @@ import styles from './Button.module.scss';
 
 describe('Button', () => {
   it('renders correctly', () => {
-    const { getByText } = render(<Button>Click me!</Button>);
-    expect(getByText('Click me!')).toBeInTheDocument();
+    const { getByRole } = render(<Button>Click me!</Button>);
+    const button = getByRole('button', { name: 'Click me!' });
+    expect(button).toBeInTheDocument();
   });
 
   it('calls onClick when clicked', () => {
     const onClick = vi.fn();
-    const { getByText } = render(<Button onClick={onClick}>Click me!</Button>);
-    const button = getByText('Click me!');
+    const { getByRole } = render(<Button onClick={onClick}>Click me!</Button>);
+    const button = getByRole('button', { name: 'Click me!' });
     fireEvent.click(button);
     expect(onClick).toHaveBeenCalledTimes(1);
   });
 
   it('renders with correct variant styles', () => {
-    const { getByText } = render(<Button variant="solid">Click me!</Button>);
-    const button = getByText('Click me!');
+    const { getByRole } = render(<Button variant="solid">Click me!</Button>);
+    const button = getByRole('button', { name: 'Click me!' });
     expect(button.classList.contains(styles['button--variant-solid'])).toBe(true);
   });
 
   it('renders with correct size styles', () => {
-    const { getByText } = render(<Button size="lg">Click me!</Button>);
-    const button = getByText('Click me!');
+    const { getByRole } = render(<Button size="lg">Click me!</Button>);
+    const button = getByRole('button', { name: 'Click me!' });
     expect(button.classList.contains(styles['button--size-lg'])).toBe(true);
   });
 
   it('renders with correct rounded styles', () => {
-    const { getByText } = render(<Button rounded>Click me!</Button>);
-    const button = getByText('Click me!');
+    const { getByRole } = render(<Button rounded>Click me!</Button>);
+    const button = getByRole('button', { name: 'Click me!' });
     expect(button.classList.contains(styles['button--rounded'])).toBe(true);
   });
 

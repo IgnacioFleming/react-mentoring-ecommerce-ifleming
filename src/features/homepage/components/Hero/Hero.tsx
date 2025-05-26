ce346a23b52bbf06a766a6a7e51319e931c55579
Emmanuel Montenegro <leunamm3@gmail.com>
2025-05-26 20:30:36 -0300

    feat: add props to Hero component

--- src/features/homepage/components/Hero/Hero.tsx
+++ src/features/homepage/components/Hero/Hero.tsx
@@ -1,21 +1,27 @@
+import { Link } from 'react-router-dom';
 import { Container } from '../../../../components/Container';
 import { Button } from '../../../../components/Button';
 
 import styles from './Hero.module.scss';
-import { Link } from 'react-router-dom';
 
-export const Hero = () => (
+export type HeroProps = {
+  eyebrow: string;
+  title: string;
+  subtitle: string;
+  cta: string;
+  ctaLink: string;
+};
+
+export const Hero = ({ eyebrow, title, subtitle, cta, ctaLink }: HeroProps) => (
   <div className={styles.hero}>
     <Container className={styles.hero__container}>
-      <h5 className={styles.hero__eyebrow}>Summer 2025</h5>
-      <h1 className={styles.hero__title}>New Collection</h1>
-      <h4 className={styles.hero__subtitle}>
-        We know how large objects will act, but things on a small scale.
-      </h4>
+      <h5 className={styles.hero__eyebrow}>{eyebrow}</h5>
+      <h1 className={styles.hero__title}>{title}</h1>
+      <h4 className={styles.hero__subtitle}>{subtitle}</h4>
       <div>
-        <Link to="/shop">
+        <Link to={ctaLink}>
           <Button size="md" className={styles.hero__button}>
-            Shop Now
+            {cta}
           </Button>
         </Link>
       </div>
--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -3,7 +3,13 @@ import { Hero } from '../../features/homepage/components/Hero';
 
 export const Homepage = () => (
   <>
-    <Hero />
+    <Hero
+      eyebrow="Summer 2025"
+      title="New Collection"
+      subtitle="We know how large objects will act, but things on a small scale."
+      cta="Shop Now"
+      ctaLink="/shop"
+    />
     <Container>
       <h3>Editor's Pick</h3>
     </Container>

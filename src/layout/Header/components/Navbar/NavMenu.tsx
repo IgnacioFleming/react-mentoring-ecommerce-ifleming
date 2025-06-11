01bfb35c26cf1dd469c6214f4814dc9ff9614809
Ignacio Fleming <ignacioflemings@gmail.com>
2025-06-11 18:20:06 -0300

    feat(layout/Header): change Navbar structure

--- src/layout/Header/components/Navbar/NavMenu.tsx
+++ src/layout/Header/components/Navbar/NavMenu.tsx
@@ -1,24 +1,18 @@
 import clsx from 'clsx';
-import { useRef } from 'react';
 import { NavLinks } from './NavLinks';
 import { NavActions } from './NavActions';
-import { useHandleOutsideClick } from '../../hooks/useHandleOutsideClick';
 import styles from './NavMenu.module.scss';
 
 type NavMenuProps = {
   open: boolean;
   closeOnClickingLink: (e: React.MouseEvent) => void;
-  handleClose: () => void;
 };
 
-export const NavMenu = ({ open, closeOnClickingLink, handleClose }: NavMenuProps) => {
-  const menuRef = useRef(null);
-  useHandleOutsideClick(menuRef, handleClose);
+export const NavMenu = ({ open, closeOnClickingLink }: NavMenuProps) => {
   return (
     <div
       className={clsx(styles.navbar__menu, open ? styles['navbar__menu--open'] : '')}
       onClick={closeOnClickingLink}
-      ref={menuRef}
     >
       <NavLinks />
       <NavActions />
--- src/layout/Header/components/Navbar/Navbar.tsx
+++ src/layout/Header/components/Navbar/Navbar.tsx
@@ -1,14 +1,22 @@
 import { Link } from 'react-router-dom';
 import styles from './Navbar.module.scss';
 import { Menu } from 'lucide-react';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { NavMenu } from './NavMenu';
+import { useHandleOutsideClick } from '../../hooks/useHandleOutsideClick';
 
 export const Navbar = () => {
   const [open, setOpen] = useState<boolean>(false);
+  const navbarRef = useRef(null);
 
-  const toggleMenu = () => setOpen(!open);
-  const handleClose = () => setOpen(false);
+  const toggleMenu = () => {
+    setOpen(!open);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  useHandleOutsideClick(navbarRef, handleClose);
 
   const closeOnClickingLink = (e: React.MouseEvent) => {
     const target = e.target as HTMLElement;
@@ -16,14 +24,14 @@ export const Navbar = () => {
   };
 
   return (
-    <>
-      <div className={styles.navbar__container}>
+    <div className={styles.navbar__container} ref={navbarRef}>
+      <div className={styles.navbar__main}>
         <div className={styles.navbar__logo}>
           <Link to="/">E-commerce</Link>
         </div>
         <Menu onClick={toggleMenu} className={styles.navbar__burguer} />
       </div>
-      <NavMenu open={open} closeOnClickingLink={closeOnClickingLink} handleClose={handleClose} />
-    </>
+      <NavMenu open={open} closeOnClickingLink={closeOnClickingLink} />
+    </div>
   );
 };

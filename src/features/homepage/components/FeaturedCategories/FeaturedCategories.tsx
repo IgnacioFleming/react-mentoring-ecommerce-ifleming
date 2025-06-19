5bd4f9e91be405ca10c3eded8f1dbe6c7c4dce60
Ignacio Fleming <ignacioflemings@gmail.com>
2025-06-19 19:09:51 -0300

    chore(features/homepage): fix styles and implementation in FeaturedCategories

--- src/features/homepage/components/FeaturedCategories/FeaturedCategories.tsx
+++ src/features/homepage/components/FeaturedCategories/FeaturedCategories.tsx
@@ -5,7 +5,7 @@ export const FeaturedCategories = () => {
   return (
     <section className={styles.container}>
       <header className={styles.header}>
-        <h3>FEATURED CATEGORIES</h3>
+        <h3>Featured Categories</h3>
         <p>Explore our most loved collections, handpicked for every style</p>
       </header>
       <FeaturedCategoriesList />
--- src/features/homepage/components/FeaturedCategories/FeaturedCategoriesList/FeaturedCategoriesList.tsx
+++ src/features/homepage/components/FeaturedCategories/FeaturedCategoriesList/FeaturedCategoriesList.tsx
@@ -5,56 +5,79 @@ import {
   sunglasses,
   smartphones,
 } from '../../../../../assets/images/featured-categories';
-import { FeaturedCategoriesItem } from '../FeaturedCategoriesItem';
+import { FeaturedCategoriesItem, FeaturedCategoriesItemProps } from '../FeaturedCategoriesItem';
 import styles from './FeaturedCategoriesList.module.scss';
 
+type Link = {
+  link: string;
+};
+
+const featuredCategories: (FeaturedCategoriesItemProps & Link)[] = [
+  {
+    imagePath: menWatches,
+    alt: "Men's watches photo",
+    title: "Men's watches",
+    caption: "Men's Watches",
+    link: '/shop/category/mens-watches',
+  },
+  {
+    imagePath: womenShoes,
+    alt: "Women's shoes photo",
+    title: "Women's shoes",
+    caption: "Women's Shoes",
+    link: '/shop/category/womens-shoes',
+  },
+  {
+    imagePath: sunglasses,
+    alt: 'Sunglasses photo',
+    title: 'Sunglasses',
+    caption: 'Sunglasses',
+    stacked: true,
+    link: '/shop/category/sunglasses',
+  },
+  {
+    imagePath: smartphones,
+    alt: 'Smartphones photo',
+    title: 'Smartphones',
+    caption: 'Smartphones',
+    stacked: true,
+    link: '/shop/category/smartphones',
+  },
+];
+
 export const FeaturedCategoriesList = () => {
+  const largeCategories = featuredCategories.filter((c) => !c.stacked);
+  const stackedCategories = featuredCategories.filter((c) => c.stacked);
   return (
     <ul className={styles['featured-categories-list']} data-testid="featured-categories-list">
-      <li>
-        <Link to="/shop/category/mens-watches">
-          <FeaturedCategoriesItem
-            imagePath={menWatches}
-            alt="Men's watches photo"
-            title="Men's watches"
-            caption="MEN'S WATCHES"
-          />
-        </Link>
-      </li>
-      <li>
-        <Link to="/shop/category/womens-shoes">
-          <FeaturedCategoriesItem
-            imagePath={womenShoes}
-            alt="Women's shoes photo"
-            title="Women's shoes"
-            caption="WOMEN'S SHOES"
-          />
-        </Link>
-      </li>
+      {largeCategories.map((c, index) => (
+        <li key={index}>
+          <Link to={c.link}>
+            <FeaturedCategoriesItem
+              imagePath={c.imagePath}
+              alt={c.alt}
+              title={c.title}
+              caption={c.caption}
+            />
+          </Link>
+        </li>
+      ))}
+
       <li>
         <ul className={styles['featured-categories-list--stacked']}>
-          <li>
-            <Link to="/shop/category/sunglasses">
-              <FeaturedCategoriesItem
-                imagePath={sunglasses}
-                alt="Sunglasses photo"
-                title="Sunglasses"
-                caption="SUNGLASSES"
-                stacked
-              />
-            </Link>
-          </li>
-          <li>
-            <Link to="/shop/category/smartphones">
-              <FeaturedCategoriesItem
-                imagePath={smartphones}
-                alt="Smartphones photo"
-                title="Smartphones"
-                caption="SMARTPHONES"
-                stacked
-              />
-            </Link>
-          </li>
+          {stackedCategories.map((c, index) => (
+            <li key={index}>
+              <Link to={c.link}>
+                <FeaturedCategoriesItem
+                  imagePath={c.imagePath}
+                  alt={c.alt}
+                  title={c.title}
+                  caption={c.caption}
+                  stacked={c.stacked}
+                />
+              </Link>
+            </li>
+          ))}
         </ul>
       </li>
     </ul>

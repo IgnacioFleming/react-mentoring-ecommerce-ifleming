ad1d676ef29f46e665ca41271b1d2857d09ebcd8
Ignacio Fleming <ignacioflemings@gmail.com>
2025-06-19 12:22:22 -0300

    style(features/homepage): add styles for FeaturedCategories

--- src/features/homepage/components/FeaturedCategories/FeaturedCategories.tsx
+++ src/features/homepage/components/FeaturedCategories/FeaturedCategories.tsx
@@ -1,37 +1,81 @@
 import { Link } from 'react-router-dom';
+import styles from './FeaturedCategories.module.scss';
+import clsx from 'clsx';
 
 export const FeaturedCategories = () => {
   return (
-    <section>
-      <header>
+    <section className={styles.container}>
+      <header className={styles.header}>
         <h3>FEATURED CATEGORIES</h3>
         <p>Explore our most loved collections, handpicked for every style</p>
       </header>
-      <ul>
-        <li>
+      <ul className={styles['featured-categories']}>
+        <li className={styles['featured-categories__item']}>
           <Link to="/shop/category/mens-watches">
-            <img src="/src/assets/images/featured-categories/men-watches.webp" alt="" />
-            <caption>MEN'S WATCHES</caption>
+            <figure>
+              <img
+                className={styles['featured-categories__item__img']}
+                src="/src/assets/images/featured-categories/men-watches.webp"
+                alt="Men's watches photo"
+                title="Men's watches"
+              />
+              <figcaption>
+                <h5>MEN'S WATCHES</h5>
+              </figcaption>
+            </figure>
           </Link>
         </li>
-        <li>
+        <li className={styles['featured-categories__item']}>
           <Link to="/shop/category/womens-shoes">
-            <img src="/src/assets/images/featured-categories/women-shoes.webp" alt="" />
-            <caption>WOMEN'S SHOES</caption>
+            <figure>
+              <img
+                className={styles['featured-categories__item__img']}
+                src="/src/assets/images/featured-categories/women-shoes.webp"
+                alt="Women's shoes photo"
+                title="Women's shoes"
+              />
+              <figcaption>
+                <h5>WOMEN'S SHOES</h5>
+              </figcaption>
+            </figure>
           </Link>
         </li>
-        <li>
-          <ul>
-            <li>
+        <li className={styles['featured-categories__item']}>
+          <ul className={styles['featured-categories__item--stacked']}>
+            <li className={styles['featured-categories__item']}>
               <Link to="/shop/category/sunglasses">
-                <img src="/src/assets/images/featured-categories/sunglasses.webp" alt="" />
-                <caption>SUNGLASSES</caption>
+                <figure>
+                  <img
+                    className={clsx(
+                      styles['featured-categories__item__img'],
+                      styles['featured-categories__item--stacked__img'],
+                    )}
+                    src="/src/assets/images/featured-categories/sunglasses.webp"
+                    alt="Sunglasses photo"
+                    title="Sunglasses"
+                  />
+                  <figcaption>
+                    <h5>SUNGLASSES</h5>
+                  </figcaption>
+                </figure>
               </Link>
             </li>
-            <li>
+            <li className={styles['featured-categories__item']}>
               <Link to="/shop/category/smartphones">
-                <img src="/src/assets/images/featured-categories/smartphones.webp" alt="" />
-                <caption>SMARTPHONES</caption>
+                <figure>
+                  <img
+                    className={clsx(
+                      styles['featured-categories__item__img'],
+                      styles['featured-categories__item--stacked__img'],
+                    )}
+                    src="/src/assets/images/featured-categories/smartphones.webp"
+                    alt="Smartphones photo"
+                    title="Smartphones"
+                  />
+                  <figcaption>
+                    <h5>SMARTPHONES</h5>
+                  </figcaption>
+                </figure>
               </Link>
             </li>
           </ul>

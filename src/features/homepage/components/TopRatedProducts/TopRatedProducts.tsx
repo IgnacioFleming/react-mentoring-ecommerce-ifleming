fb781f92de557930bde07952f107b0d140f3c4cb
Ignacio Fleming <ignacioflemings@gmail.com>
2025-06-23 18:46:43 -0300

    feat(features/homepage): add Card component to TopRatedProducts

--- src/features/homepage/components/TopRatedProducts/TopRatedProducts.tsx
+++ src/features/homepage/components/TopRatedProducts/TopRatedProducts.tsx
@@ -1,3 +1,9 @@
+import { Link } from 'react-router-dom';
+import { Card } from '../../../../components/Card';
+import { products } from './products';
+import { Button } from '../../../../components/Button';
+import { Eye, Heart, ShoppingCart, Star } from 'lucide-react';
+
 export const TopRatedProducts = () => {
   return (
     <section>
@@ -6,7 +12,38 @@ export const TopRatedProducts = () => {
         <h3>Top Rated Products</h3>
         <p>Customer favorites loved for quality and style</p>
       </header>
-      <ul></ul>
+      <ul>
+        {products.map((p, index) => (
+          <Card key={index}>
+            <Card.Thumbnail src={p.thumbnail} alt={`${p.name} photo`}>
+              <div>
+                <Heart />
+                <ShoppingCart />
+                <Eye />
+              </div>
+            </Card.Thumbnail>
+            <Card.Content>
+              <Card.Header>
+                <Link to="">{p.brand}</Link>
+                <div>
+                  <Star />
+                  <small>{p.rating}</small>
+                </div>
+              </Card.Header>
+              <Card.Main>
+                <h5>{p.name}</h5>
+                <div>
+                  <h5>{p.price}</h5>
+                  <h5>{p.discount}</h5>
+                </div>
+              </Card.Main>
+              <Card.Footer>
+                <Button>Read more</Button>
+              </Card.Footer>
+            </Card.Content>
+          </Card>
+        ))}
+      </ul>
     </section>
   );
 };

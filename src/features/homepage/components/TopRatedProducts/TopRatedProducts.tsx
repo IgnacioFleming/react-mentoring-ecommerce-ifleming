bdb0d221bb06cc9839ab57ac4f2fc7ce66dede54
Ignacio Fleming <ignacioflemings@gmail.com>
2025-06-24 11:22:42 -0300

    style(features/homepage): add styles for container and header in TopRatedProducts component

--- src/features/homepage/components/TopRatedProducts/TopRatedProducts.tsx
+++ src/features/homepage/components/TopRatedProducts/TopRatedProducts.tsx
@@ -1,13 +1,14 @@
 import { Link } from 'react-router-dom';
-import { Card } from '../../../../components/Card';
-import { PRODUCTS } from './products';
-import { Button } from '../../../../components/Button';
 import { Eye, Heart, ShoppingCart, Star } from 'lucide-react';
+import { Card } from '../../../../components/Card';
+import { Button } from '../../../../components/Button';
+import { PRODUCTS } from './products';
+import styles from './TopRatedProducts.module.scss';
 
 export const TopRatedProducts = () => {
   return (
-    <section>
-      <header>
+    <section className={styles.container}>
+      <header className={styles.header}>
         <h4>Featured Products</h4>
         <h3>Top Rated Products</h3>
         <p>Customer favorites loved for quality and style</p>

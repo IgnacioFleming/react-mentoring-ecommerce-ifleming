a118a7075b5be0dcf4019860d79e7bd774eefb33
Ignacio Fleming <ignacioflemings@gmail.com>
2025-06-24 16:29:14 -0300

    style(features/homepage): add styles for TopRatedProducts component

--- src/features/homepage/components/TopRatedProducts/TopRatedProducts.tsx
+++ src/features/homepage/components/TopRatedProducts/TopRatedProducts.tsx
@@ -1,7 +1,4 @@
-import { Link } from 'react-router-dom';
-import { Eye, Heart, ShoppingCart, Star } from 'lucide-react';
-import { Card } from '../../../../components/Card';
-import { Button } from '../../../../components/Button';
+import { ProductCard } from './ProductCard';
 import { PRODUCTS } from './products';
 import styles from './TopRatedProducts.module.scss';
 
@@ -13,36 +10,11 @@ export const TopRatedProducts = () => {
         <h3>Top Rated Products</h3>
         <p>Customer favorites loved for quality and style</p>
       </header>
-      <ul>
-        {PRODUCTS.slice(0, 1).map((p, index) => (
-          <Card key={index}>
-            <Card.Thumbnail src={p.thumbnail} alt={`${p.name} photo`}>
-              <div>
-                <Heart />
-                <ShoppingCart />
-                <Eye />
-              </div>
-            </Card.Thumbnail>
-            <Card.Content>
-              <Card.Header>
-                <Link to="">{p.brand}</Link>
-                <div>
-                  <Star />
-                  <small>{p.rating}</small>
-                </div>
-              </Card.Header>
-              <Card.Main>
-                <h5>{p.name}</h5>
-                <div>
-                  <h5>{p.price}</h5>
-                  <h5>{p.discount}</h5>
-                </div>
-              </Card.Main>
-              <Card.Footer>
-                <Button>Read more</Button>
-              </Card.Footer>
-            </Card.Content>
-          </Card>
+      <ul className={styles['top-rated-products']}>
+        {PRODUCTS.map((p, index) => (
+          <li key={index}>
+            <ProductCard product={p} />
+          </li>
         ))}
       </ul>
     </section>

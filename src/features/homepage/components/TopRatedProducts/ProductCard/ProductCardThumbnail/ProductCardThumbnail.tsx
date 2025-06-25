14c5da9cc3cb7b61e41b4d9e7f96f14a158dec3c
Ignacio Fleming <ignacioflemings@gmail.com>
2025-06-25 19:39:25 -0300

    style(features/homepage): remove wrapers for ProductCardThumbnail icons

--- src/features/homepage/components/TopRatedProducts/ProductCard/ProductCardThumbnail/ProductCardThumbnail.tsx
+++ src/features/homepage/components/TopRatedProducts/ProductCard/ProductCardThumbnail/ProductCardThumbnail.tsx
@@ -7,20 +7,12 @@ import styles from './ProductCardThumbnail.module.scss';
 
 type ProductCardThumbnailProps = Pick<Product, 'thumbnail' | 'name'>;
 
-const ICON_SIZE = 20;
-
 export const ProductCardThumbnail = ({ thumbnail, name }: ProductCardThumbnailProps) => (
   <Card.Thumbnail src={thumbnail} alt={`${name} photo`} className={styles.thumbnail}>
     <div className={clsx(hoverStyles.thumbnail__icons, styles.thumbnail__icons)}>
-      <div>
-        <Heart size={ICON_SIZE} />
-      </div>
-      <div>
-        <ShoppingCart size={ICON_SIZE} />
-      </div>
-      <div>
-        <Eye size={ICON_SIZE} />
-      </div>
+      <Heart />
+      <ShoppingCart />
+      <Eye />
     </div>
   </Card.Thumbnail>
 );

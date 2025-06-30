1a8369b2af2e216e178fa0526c126ac49cac61d6
Ignacio Fleming <ignacioflemings@gmail.com>
2025-06-30 14:11:18 -0300

    feat(components/ProductCard): add Skeleton components to ProductCard

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -6,33 +6,51 @@ import { ProductCardThumbnail } from './ProductCardThumbnail';
 import { ProductCardHeader } from './ProductCardHeader';
 import { ProductCardMain } from './ProductCardMain';
 import styles from './ProductCard.module.scss';
+import { Skeleton } from '../Skeleton';
 
 type ProductCardProps = {
-  product: Product;
+  product: Product | null;
 };
 
-export const ProductCard = ({ product }: ProductCardProps) => (
-  <li>
-    <Card className={styles.card}>
-      <ProductCardThumbnail thumbnail={product.thumbnail} name={product.title} />
-      <Card.Content className={styles.card__content}>
-        <ProductCardHeader brand={product.brand} rating={product.rating} />
-        <ProductCardMain
-          name={product.title}
-          price={product.price}
-          discount={product.discountPercentage}
-        />
-        <Card.Footer>
-          <Button
-            variant="outline"
-            rounded
-            rightIcon={<ChevronRight size={24} />}
-            className={styles.card__content__footer__btn}
-          >
-            Learn More
-          </Button>
-        </Card.Footer>
-      </Card.Content>
-    </Card>
-  </li>
-);
+export const ProductCard = ({ product }: ProductCardProps) => {
+  return (
+    <li className={styles.container}>
+      <Card className={styles.card}>
+        {product ? (
+          <ProductCardThumbnail thumbnail={product?.thumbnail} title={product?.title} />
+        ) : (
+          <Skeleton className={styles['card__thumbnail-skeleton']} />
+        )}
+        <Card.Content className={styles.card__content}>
+          {product ? (
+            <>
+              <ProductCardHeader brand={product.brand} rating={product.rating} />
+              <ProductCardMain
+                name={product.title}
+                price={product.price}
+                discount={product.discountPercentage}
+              />
+              <Card.Footer>
+                <Button
+                  variant="outline"
+                  rounded
+                  rightIcon={<ChevronRight size={24} />}
+                  className={styles.card__content__footer__btn}
+                >
+                  Learn More
+                </Button>
+              </Card.Footer>
+            </>
+          ) : (
+            <>
+              <Skeleton className={styles['card__content-skeleton']} />
+              <Skeleton className={styles['card__content-skeleton']} />
+              <Skeleton className={styles['card__content-skeleton']} />
+              <Skeleton className={styles['card__content-skeleton--button']} />
+            </>
+          )}
+        </Card.Content>
+      </Card>
+    </li>
+  );
+};

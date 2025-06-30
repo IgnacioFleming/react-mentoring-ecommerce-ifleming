cfc9e13a26c609403902c8ef05d38e713cc691ba
Ignacio Fleming <ignacioflemings@gmail.com>
2025-06-30 14:20:13 -0300

    test(components/Skeleton): add tests for Skeleton component

---
+++ src/components/Skeleton/Skeleton.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+import { Skeleton } from './Skeleton';
+
+describe('Skeleton', () => {
+  it('renders Skeleton component with the correct styles', () => {
+    const { container } = render(<Skeleton />);
+    screen.debug();
+    expect(container.firstChild).toHaveClass(/skeleton/);
+  });
+});
--- src/components/Skeleton/Skeleton.tsx
+++ src/components/Skeleton/Skeleton.tsx
@@ -1,7 +1,7 @@
 import clsx from 'clsx';
 import styles from './Skeleton.module.scss';
 
-type SkeletonProps = { className: string };
+type SkeletonProps = { className?: string };
 
 export const Skeleton = ({ className }: SkeletonProps) => (
   <div className={clsx(styles.skeleton, className)} />

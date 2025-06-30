aac44f2554f8041340428d2fd1ddd1520b940ee8
Ignacio Fleming <ignacioflemings@gmail.com>
2025-06-30 14:28:16 -0300

    test(componets/Skeleton): update Skeleton test

--- src/components/Skeleton/Skeleton.test.tsx
+++ src/components/Skeleton/Skeleton.test.tsx
@@ -1,12 +1,13 @@
-import { render, screen } from '@testing-library/react';
-
+import { render } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 import { Skeleton } from './Skeleton';
 
 describe('Skeleton', () => {
   it('renders Skeleton component with the correct styles', () => {
+    const { getByTestId } = render(<Skeleton />);
+    expect(getByTestId('skeleton')).toBeInTheDocument();
+    expect(getByTestId('skeleton')).toHaveClass(/skeleton/);
     const { container } = render(<Skeleton />);
-    screen.debug();
     expect(container.firstChild).toHaveClass(/skeleton/);
   });
 });

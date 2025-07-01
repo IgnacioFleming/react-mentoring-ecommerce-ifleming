b3d0a697bbbc2abb852dcd2808bec871fe0caa1e
Ignacio Fleming <ignacioflemings@gmail.com>
2025-07-01 16:11:20 -0300

    feat(lib/api): add composeQueryParams utility function

---
+++ src/lib/api/utils.ts
@@ -0,0 +1,13 @@
+export const composeQueryParams = <T>(params: Partial<T>): string => {
+  const entries = Object.entries(params);
+  let paramsString = entries.length > 0 ? '/?' : '';
+
+  if (entries.length > 0) {
+    entries.forEach(([key, value], index) => {
+      let param = `${key}=${value}`;
+      if (index < entries.length - 1) param += '&';
+      paramsString += param;
+    });
+  }
+  return paramsString;
+};

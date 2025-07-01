d8f13bf25697e3d27f8c3a27244da6edd02987f4
Ignacio Fleming <ignacioflemings@gmail.com>
2025-07-01 15:41:51 -0300

    chore(features/hooks): add generic hook to get products

---
+++ src/features/hooks/useProducts.ts
@@ -0,0 +1,33 @@
+import { useQuery } from '@tanstack/react-query';
+import { DataStatus } from '../../types/products';
+import { getProducts } from '../../lib/api/products';
+import { STALE_TIMES } from '../../lib/api/config';
+
+export const useProducts = (params: string) => {
+  const {
+    data: { products } = { products: [] },
+    isLoading: isLoadingProducts,
+    error: productsError,
+    isError: isErrorProducts,
+    ...rest
+  } = useQuery({
+    queryKey: ['topRatedProducts'],
+    queryFn: () => getProducts({ params }),
+    staleTime: STALE_TIMES.DEFAULT,
+  });
+
+  const getQueryStatus = (): DataStatus => {
+    if (isErrorProducts) return 'error';
+    if (isLoadingProducts) return 'loading';
+    return 'success';
+  };
+
+  return {
+    products,
+    isLoadingProducts,
+    productsError,
+    isErrorProducts,
+    getQueryStatus,
+    ...rest,
+  };
+};

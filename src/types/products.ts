00dfb2736c82cdd51262b1993afe8b8562714331
Ignacio Fleming <ignacioflemings@gmail.com>
2025-07-01 16:15:14 -0300

    feat(types): add QueryParams types

--- src/types/products.ts
+++ src/types/products.ts
@@ -20,3 +20,13 @@ export const PRODUCT_DATA_STATUS = {
 } as const;
 
 export type DataStatus = (typeof PRODUCT_DATA_STATUS)[keyof typeof PRODUCT_DATA_STATUS];
+
+interface QueryParams {
+  sortBy: string;
+  order: 'asc' | 'desc';
+  limit: number;
+}
+
+export interface ProductQueryParams extends QueryParams {
+  sortBy: keyof Product;
+}

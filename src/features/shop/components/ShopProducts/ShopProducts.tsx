9b4e11dc39ecb73fdd805bbf8c60a68be571ba0e
Ignacio Fleming <ignacioflemings@gmail.com>
2025-07-04 14:12:52 -0300

    feat(features/shop): add infinite scroll logic to ShopProducts component

--- src/features/shop/components/ShopProducts/ShopProducts.tsx
+++ src/features/shop/components/ShopProducts/ShopProducts.tsx
@@ -1,20 +1,48 @@
+import { useEffect, useRef, useState } from 'react';
+import { Product } from '../../../../types/products';
 import { ProductList } from '../../../../components/ProductList';
 import { useProducts } from '../../../hooks/useProducts';
 import styles from './ShopProducts.module.scss';
+import { useInfiniteScroll } from '../../hooks/useInfiniteScroll';
 
 export const ShopProducts = () => {
-  const limit = 24;
-  const { products, getQueryStatus, displayedProductQuantity } = useProducts({
-    limit,
+  const [skip, setSkip] = useState<number>(0);
+  const [shopProducts, setShopProducts] = useState<Product[]>([]);
+  const sentinelRef = useRef<HTMLDivElement>(null);
+  const LIMIT = 24;
+  const {
+    products,
+    getQueryStatus,
+    displayedProductQuantity,
+    refetch,
+    hasMore,
+    isLoadingProducts,
+    isFetching,
+  } = useProducts({
+    limit: LIMIT,
+    skip,
   });
+  const handleSetSkip = () => setSkip((prevSkip) => prevSkip + LIMIT);
 
+  useInfiniteScroll({ hasMore, setOffset: handleSetSkip, loadMore: refetch, ref: sentinelRef });
   const productDataStatus = getQueryStatus();
+
+  useEffect(() => {
+    setShopProducts((prevProducts) => prevProducts.concat(products));
+  }, [products]);
+
   return (
     <>
       <div className={styles['shop-products__header']}>
         {productDataStatus === 'success' && <h6>Showing all {displayedProductQuantity} results</h6>}
       </div>
-      <ProductList products={products} skeletonQuantity={limit} status={productDataStatus} />
+      <ProductList products={shopProducts} skeletonQuantity={LIMIT} status={productDataStatus} />
+      {!isLoadingProducts && isFetching && (
+        <div className={styles['shop-products__loader']}>
+          <ProductList products={[]} skeletonQuantity={LIMIT} status="loading" />
+        </div>
+      )}
+      <div ref={sentinelRef} />
     </>
   );
 };

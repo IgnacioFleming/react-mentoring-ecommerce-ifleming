70df6c1ddb052c9c51c5d76e7d7c3a6765e943a3
Ignacio Fleming <ignacioflemings@gmail.com>
2025-07-08 20:52:04 -0300

    feat(features/shop): add useIntersectionObserver custom hook

---
+++ src/features/shop/hooks/useIntersectionObserver.ts
@@ -0,0 +1,33 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+
+import { RefObject, useEffect, useRef } from 'react';
+
+type UseIntersectionObserverProps = {
+  ref: RefObject<HTMLElement | null>;
+  cb: (entries: IntersectionObserverEntry[]) => void;
+  rootMargin?: `${number}px`;
+  threshold?: number;
+};
+
+export const useIntersectionObserver = ({
+  ref,
+  cb,
+  rootMargin = '0px',
+  threshold = 0,
+}: UseIntersectionObserverProps) => {
+  const observerRef = useRef<IntersectionObserver | null>(null);
+
+  useEffect(() => {
+    if (!ref.current) return;
+
+    if (observerRef.current) observerRef.current.disconnect();
+
+    observerRef.current = new IntersectionObserver(cb, { rootMargin, threshold });
+
+    observerRef.current.observe(ref.current);
+
+    return () => {
+      observerRef.current?.disconnect();
+    };
+  }, []);
+};

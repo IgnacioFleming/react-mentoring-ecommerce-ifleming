979803ff2b8a04b2192e50e166688bcf03d5723d
Ignacio Fleming <ignacioflemings@gmail.com>
2025-07-09 15:53:19 -0300

    chore(features/shop): refactor ShopProducts hooks

--- src/features/shop/components/ShopProducts/ShopProducts.tsx
+++ src/features/shop/components/ShopProducts/ShopProducts.tsx
@@ -1,35 +1,31 @@
-import { useRef, useState } from 'react';
+import { useRef } from 'react';
 import { Product } from '../../../../types/products';
 import { useProducts } from '../../../hooks/useProducts';
 import { useInfiniteScroll } from '../../hooks/useInfiniteScroll';
 import { useIntersectionObserver } from '../../hooks/useIntersectionObserver';
-import { useHandleIntersect } from '../../hooks/useHandleIntersect';
+import { useHandleOffset } from '../../hooks/useHandleOffset';
 import { ProductList } from '../../../../components/ProductList';
 import styles from './ShopProducts.module.scss';
 
 const LIMIT = 24;
 
 export const ShopProducts = () => {
-  const [skip, setSkip] = useState<number>(0);
   const sentinelRef = useRef<HTMLDivElement>(null);
+  const { offset, handleOffset } = useHandleOffset(LIMIT);
 
   const { products, getQueryStatus, total, refetch } = useProducts({
-    params: { limit: LIMIT, skip },
+    params: { limit: LIMIT, skip: offset },
     queryKey: 'shopProducts',
   });
 
-  const handleIntersect = useHandleIntersect({
-    setOffset: () => setSkip((prevSkip) => prevSkip + LIMIT),
-  });
-
-  useIntersectionObserver({ ref: sentinelRef, cb: handleIntersect, rootMargin: '-200px' });
-
   const { accItems: shopProducts, loading } = useInfiniteScroll<Product>({
     loadMore: refetch,
-    offset: skip,
+    offset,
     newItems: products,
     totalItems: total,
   });
+
+  useIntersectionObserver({ ref: sentinelRef, cb: handleOffset, rootMargin: '-200px' });
 
   const productDataStatus = getQueryStatus();
 

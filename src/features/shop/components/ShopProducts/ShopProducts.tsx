3d4cccdc6b63b1e30813375598a5013853690ec4
Ignacio Fleming <ignacioflemings@gmail.com>
2025-07-10 15:37:56 -0300

    feat(features/shop): add ShopProductLoader component and implemented it on ShopProducsts

--- src/features/shop/components/ShopProducts/ShopProducts.tsx
+++ src/features/shop/components/ShopProducts/ShopProducts.tsx
@@ -1,10 +1,11 @@
 import { useRef } from 'react';
 import { Product } from '../../../../types/products';
 import { useProducts } from '../../../hooks/useProducts';
-import { useInfiniteScroll } from '../../hooks/useInfiniteScroll';
-import { useIntersectionObserver } from '../../hooks/useIntersectionObserver';
-import { useHandleOffset } from '../../hooks/useHandleOffset';
+import { useInfiniteScroll } from '../../../hooks/useInfiniteScroll';
+import { useIntersectionObserver } from '../../../hooks/useIntersectionObserver';
+import { useHandleOffset } from '../../../hooks/useHandleOffset';
 import { ProductList } from '../../../../components/ProductList';
+import { ShopProductLoader } from './ShopProductsLoader';
 import styles from './ShopProducts.module.scss';
 
 const LIMIT = 24;
@@ -25,9 +26,12 @@ export const ShopProducts = () => {
     totalItems: total,
   });
 
-  useIntersectionObserver({ ref: sentinelRef, cb: handleOffset, rootMargin: '-200px' });
+  useIntersectionObserver({ ref: sentinelRef, cb: handleOffset, rootMargin: '0px' });
 
   const productDataStatus = getQueryStatus();
+
+  const productLeft = total - shopProducts.length;
+  const skeletonQuantity = productLeft > LIMIT ? LIMIT : productLeft;
 
   return (
     <>
@@ -35,11 +39,7 @@ export const ShopProducts = () => {
         {productDataStatus === 'success' && <h6>Showing all {total} results</h6>}
       </header>
       <ProductList products={shopProducts} skeletonQuantity={LIMIT} status={productDataStatus} />
-      {loading && (
-        <div className={styles['shop-products__loader']}>
-          <ProductList products={[]} skeletonQuantity={LIMIT} status="loading" />
-        </div>
-      )}
+      <ShopProductLoader loading={loading} quantity={skeletonQuantity} />
       <div ref={sentinelRef} />
     </>
   );

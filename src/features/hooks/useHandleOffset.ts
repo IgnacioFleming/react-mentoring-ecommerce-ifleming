07ddca82d92b5a7c3b1c2eca63a56d169568130b
Ignacio Fleming <ignacioflemings@gmail.com>
2025-07-10 15:33:39 -0300

    feat(features/shop): rename useHandleOffset and remove firstRender flag

--- src/features/hooks/useHandleOffset.ts
+++ src/features/hooks/useHandleOffset.ts
@@ -1,14 +1,12 @@
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 
 export const useHandleOffset = (incremental = 1) => {
-  const firstRender = useRef(true);
   const [offset, setOffset] = useState<number>(0);
   const handleOffset = (entries: IntersectionObserverEntry[]) => {
     const entry = entries[0];
-    if (entry.isIntersecting && !firstRender.current) {
+    if (entry.isIntersecting) {
       setOffset((prevOffset) => prevOffset + incremental);
     }
-    if (entry.isIntersecting && firstRender.current) return (firstRender.current = false);
   };
   return { offset, handleOffset };
 };

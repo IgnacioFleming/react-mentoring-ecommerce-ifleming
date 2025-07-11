95485884a8113f79fa1b4afc65ae17547329de38
Ignacio Fleming <ignacioflemings@gmail.com>
2025-07-11 10:07:03 -0300

    feat(components/Dropdown): add Dropdown global component

---
+++ src/components/Dropdown/Dropdown.tsx
@@ -0,0 +1,43 @@
+import * as DropdownPrimitive from '@radix-ui/react-dropdown-menu';
+
+export const Dropdown = ({
+  children,
+  className,
+}: DropdownPrimitive.DropdownMenuProps & { className: string }) => (
+  <div className={className}>
+    <DropdownPrimitive.Root>{children}</DropdownPrimitive.Root>
+  </div>
+);
+
+Dropdown.displayName = 'Dropdown';
+
+const Trigger = ({ children, className }: DropdownPrimitive.DropdownMenuTriggerProps) => (
+  <DropdownPrimitive.DropdownMenuTrigger className={className} asChild>
+    {children}
+  </DropdownPrimitive.DropdownMenuTrigger>
+);
+
+Dropdown.Trigger = Trigger;
+
+const Content = ({
+  children,
+  className,
+}: DropdownPrimitive.DropdownMenuContentProps & DropdownPrimitive.DropdownMenuPortalProps) => (
+  <DropdownPrimitive.Portal>
+    <DropdownPrimitive.Content className={className}>{children}</DropdownPrimitive.Content>
+  </DropdownPrimitive.Portal>
+);
+
+Dropdown.Content = Content;
+
+const Item = ({ children, className }: DropdownPrimitive.DropdownMenuItemProps) => (
+  <DropdownPrimitive.Item className={className}>{children}</DropdownPrimitive.Item>
+);
+
+Dropdown.Item = Item;
+
+const Separator = ({ className }: DropdownPrimitive.DropdownMenuSeparatorProps) => (
+  <DropdownPrimitive.DropdownMenuSeparator className={className} />
+);
+
+Dropdown.Separator = Separator;

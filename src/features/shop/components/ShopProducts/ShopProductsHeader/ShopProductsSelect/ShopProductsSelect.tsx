e3214b22b3baf8e1fe4e5cdaaf74d48e55ddf586
Ignacio Fleming <ignacioflemings@gmail.com>
2025-07-14 17:37:56 -0300

    feat(features/shop): add ShopProductsSelect component

---
+++ src/features/shop/components/ShopProducts/ShopProductsHeader/ShopProductsSelect/ShopProductsSelect.tsx
@@ -0,0 +1,45 @@
+import clsx from 'clsx';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { ChevronDown } from 'lucide-react';
+import { Select } from '../../../../../../components/Select';
+import styles from './ShopProductsSelect.module.scss';
+
+const ORDER = [
+  { value: 'Default', path: '' },
+  { value: 'Top Rated', path: '?orderBy=top-rated' },
+  { value: 'Price Desc', path: '?orderBy=price-desc' },
+  { value: 'Price Asc', path: '?orderBy=price-asc' },
+] as const;
+
+export const ShopProductsSelect = () => {
+  const navigate = useNavigate();
+  const { search } = useLocation();
+
+  const defaultValue = ORDER.find((o) => o.path === search)?.value || 'Default';
+
+  const handleChange = (value: (typeof ORDER)[number]['value']) => {
+    const path = ORDER.find((o) => o.value === value)?.path;
+    if (path) navigate(path);
+    else navigate('');
+  };
+
+  return (
+    <Select className={styles.select} onValueChange={handleChange}>
+      <Select.Trigger
+        className={clsx(styles.select__option, styles.select__trigger, styles.select__value)}
+      >
+        <Select.Value placeholder={defaultValue} />
+        <Select.Icon className={styles.select__trigger__icon}>
+          <ChevronDown />
+        </Select.Icon>
+      </Select.Trigger>
+      <Select.Content className={styles.select__content}>
+        {ORDER.map(({ value }, index) => (
+          <Select.Item key={index} value={value} className={styles.select__option}>
+            {value}
+          </Select.Item>
+        ))}
+      </Select.Content>
+    </Select>
+  );
+};

32840a5d0f38d5b07e70397024d53b9658e73e63
Ignacio Fleming <ignacioflemings@gmail.com>
2025-07-15 10:12:44 -0300

    feat: add export to queryClient

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './styles/index.scss';
 import App from './App.tsx';
 
-const queryClient = new QueryClient();
+export const queryClient = new QueryClient();
 window.__TANSTACK_QUERY_CLIENT__ = queryClient;
 
 createRoot(document.getElementById('root')!).render(

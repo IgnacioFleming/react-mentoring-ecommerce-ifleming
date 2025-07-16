4b1a7d849055160a967bf6622061a6c6196e80d1
Ignacio Fleming <ignacioflemings@gmail.com>
2025-07-16 16:03:49 -0300

    chore(components/ProductList): refactor ProductList component to handle more products loading

--- src/components/ProductList/ProductList.tsx
+++ src/components/ProductList/ProductList.tsx
@@ -8,20 +8,24 @@ export type ProductListProps = {
   products: Product[];
   status: DataStatus;
   skeletonQuantity: number;
+  isLoadingMore?: boolean;
 };
 
-export const ProductList = ({ products, status, skeletonQuantity }: ProductListProps) => {
+export const ProductList = ({
+  products,
+  status,
+  skeletonQuantity,
+  isLoadingMore = false,
+}: ProductListProps) => {
   const isError = status === PRODUCT_DATA_STATUS.ERROR;
-  const isLoading = status === PRODUCT_DATA_STATUS.LOADING;
+  const isLoading = status === PRODUCT_DATA_STATUS.LOADING || isLoadingMore;
+  const isSuccess = status === PRODUCT_DATA_STATUS.SUCCESS;
 
   if (isError) return <ProductListError />;
   return (
     <ul className={styles['products-list']}>
-      {isLoading ? (
-        <ProductListSkeleton quantity={skeletonQuantity} />
-      ) : (
-        products.map((p, index) => <ProductCard key={index} product={p} />)
-      )}
+      {isSuccess && products.map((p, index) => <ProductCard key={index} product={p} />)}
+      {isLoading && <ProductListSkeleton quantity={skeletonQuantity} />}
     </ul>
   );
 };

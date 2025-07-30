d513880a2b6faba69ef42b98919b0a5753831e8a
Ignacio Fleming <ignacioflemings@gmail.com>
2025-07-30 20:34:25 -0300

    feat(pages/ProductDetailSection): add error and loading components to page

--- src/pages/ProductDetailSection/ProductDetailSection.tsx
+++ src/pages/ProductDetailSection/ProductDetailSection.tsx
@@ -1,20 +1,31 @@
 import { useParams } from 'react-router-dom';
 import { useGetProductById } from './hooks/useGetProductById';
-import { ProductDetail } from '@/components/ProductDetail';
 import { Container } from '@/components/Container';
+import { ErrorSection } from '@/components/ErrorSection';
+import { ProductDetail } from '@/components/ProductDetail';
+import { ProductDetailSkeleton } from '@/components/ProductDetail/ProductDetailSkeleton';
 import styles from './ProductDetailSection.module.scss';
 
 export const ProductDetailSection = () => {
   const id = Number(useParams().id);
-  const { product, isError, isLoading } = useGetProductById(id);
-  if (isError) return null;
-  if (isLoading) return null;
+  const { product, dataStatus } = useGetProductById(id);
+
+  const renderProductDetail = () => {
+    if (dataStatus === 'error')
+      return (
+        <ErrorSection
+          title="Loading Error:"
+          message="There was an error while loading Product Detail"
+          goBackBtn
+        />
+      );
+    if (dataStatus === 'loading') return <ProductDetailSkeleton />;
+    return <ProductDetail product={product} />;
+  };
 
   return (
     <div className={styles.wrapper}>
-      <Container>
-        <ProductDetail product={product} />
-      </Container>
+      <Container>{renderProductDetail()}</Container>
     </div>
   );
 };

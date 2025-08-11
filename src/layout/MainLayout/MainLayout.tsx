de891f77dfb80cc19fcbdb9a6b29b5bc50343329
Ignacio Fleming <ignacioflemings@gmail.com>
2025-08-11 16:36:56 -0300

    feat(layout/hooks): add useScrollTop for smooth top scrolling on every new route

--- src/layout/MainLayout/MainLayout.tsx
+++ src/layout/MainLayout/MainLayout.tsx
@@ -1,15 +1,18 @@
 import { Outlet } from 'react-router-dom';
+import { useScrollTop } from '../hooks/useScrollTop';
 import { Header } from '../Header';
 import { Footer } from '../Footer';
-
 import styles from './MainLayout.module.scss';
 
-export const MainLayout = () => (
-  <div className={styles.layout}>
-    <Header />
-    <main className={styles.layout__main}>
-      <Outlet />
-    </main>
-    <Footer />
-  </div>
-);
+export const MainLayout = () => {
+  useScrollTop();
+  return (
+    <div className={styles.layout}>
+      <Header />
+      <main className={styles.layout__main}>
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+};
